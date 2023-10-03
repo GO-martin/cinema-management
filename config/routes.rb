17068289238b96d0b8627e12c6f70ffd0490ae10
GO-martin <martin.nguyen.goldenowl@gmail.com>
2023-10-03 12:28:33 +0700

    custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "dashboard", to: "pages#dashboard"
+  get "home_admin", to: "pages#home_admin"
   resources :cinemas
+
+  # Custom Error Pages
+  match "/404", to: "errors#file_not_found", via: :all
+  match "/422", to: "errors#unprocessable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
+
+
 end
